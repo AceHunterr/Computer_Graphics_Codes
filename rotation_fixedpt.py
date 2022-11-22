Added Rotation Fixed

rotation_fixedpt.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import math
 
 import numpy as np
-pi=3.1416
+
 # numpy to handle the arrays or matrix
 x1=int(input("x1: "))
 y1=int(input("y1: "))
@@ -12,23 +12,41 @@ x3=int(input("x3: "))
 y3=int(input("y3: "))
 x4=int(input("x4: "))
 y4=int(input("y4: "))
-X = [x1, x2, x3, x1]
-Y = [y1, y2, y3, y1]
+X = [x1, x2, x3, x4 , x1]
+Y = [y1, y2, y3, y4 , y1]
+
 
 # translate
 theta = float(input("Enter rotating variable "))
 
-
 s = math.sin(math.radians(theta))
 c = math.cos(math.radians(theta))
 
+x_trans=int(input("fixed point x coordinate: "))
+y_trans=int(input("fixed point y coordinate: "))
+X_new=[]
+Y_new=[]
+
+T1=[[1,0,-x_trans],
+   [0,1,-y_trans],
+   [0,0,1]]
+
+T2=[[c,-s,0],
+    [s,c,0],
+    [0,0,1]]
+
+T3=[[1,0,x_trans],
+   [0,1,y_trans],
+   [0,0,1]]
+
+t = np.dot(T3,T2)
+T = np.dot(t,T1)
 X_new = []
 Y_new = []
 
+
+
 R = [0, 0, 0]
-T = [[c, -s, 0],
-     [s, c, 0],
-     [0, 0, 1]]
 
 
 for i in range(len(X)):

scaling.py
@@ -12,28 +12,33 @@ X=[x1,x2,x3,x1]
 Y=[y1,y2,y3,y1]
 plt.plot(X,Y)
 plt.show()
-
-
-#Translation
-x_trans=int(input("x-coordinate "))
-y_trans=int(input("y-coordinate "))
-
+S=int(input("Enter scaling factor "))
+x_fix=int(input("x coordinate to be fixed "))
+y_fix=int(input("y coordinate to be fixed "))
 
 
 
 X_new=[]
 Y_new=[]
 
-T=[[1, 0, x_trans],
-   [0, 1, y_trans],
+T=[[1, 0, x_fix],
+   [0, 1, y_fix],
+    [0, 0, 1]]
+S=[[S, 0, 0],
+   [0, S, 0],
+    [0, 0, 1]]
+Tn=[[1, 0, -x_fix],
+   [0, 1, -y_fix],
     [0, 0, 1]]
 
 R=[0, 0, 0]
+tk=np.dot(T,S)
+tf=np.dot(tk,Tn)
 
             
 for i in range(len(X)):
     B=[X[i],Y[i],1]
-    R=np.dot(T,B)
+    R=np.dot(tf,B)
     X_new.append(R[0])
     Y_new.append(R[1])
     
@@ -44,7 +49,6 @@ Y_new
 
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
 
 plt.plot(X,Y,X_new,Y_new)
 plt.show()